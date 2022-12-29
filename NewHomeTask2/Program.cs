347361a1f1b830390ab0c65813c2603a97d434ee
kenanxasiyevv <kananakh@code.edu.az>
2022-12-29 15:14:43 +0400

    Add project files.

---
+++ NewHomeTask2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace NewHomeTask2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ededi daxil edin: ");
+            int number = int.Parse(Console.ReadLine());
+            int r;
+            int count = 0;
+            while (number >= 9)
+            {
+                r = number % 10;
+                number /= 10;
+                if (r == 0)
+                    count++;
+            }Console.WriteLine(count);
+        }
+    }
+}
